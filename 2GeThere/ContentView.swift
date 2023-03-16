5e71ebcb7f864c6cd54702aefa51b75c2dcd4bc7
haikallf <haikallzrd@gmail.com>
2023-03-17 01:07:10 +0700

    init

---
+++ 2GeThere/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  2GeThere
+//
+//  Created by Haikal Lazuardi Fadil on 17/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2GeThere/_GeThereApp.swift
@@ -0,0 +1,17 @@
+//
+//  _GeThereApp.swift
+//  2GeThere
+//
+//  Created by Haikal Lazuardi Fadil on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct _GeThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
