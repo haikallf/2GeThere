929b3de33e7262140bcff2a7fcbf056fc801a545
haikallf <haikallzrd@gmail.com>
2023-03-17 02:21:04 +0700

    add view nav

---
+++ 2GeThere/AuthView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthView.swift
+//  2GeThere
+//
+//  Created by Haikal Lazuardi Fadil on 17/03/23.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
