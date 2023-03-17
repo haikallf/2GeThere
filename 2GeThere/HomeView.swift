857cdc3d27d90ebcc2a138bdde6e3d9e92b44771
haikallf <haikallzrd@gmail.com>
2023-03-18 00:09:55 +0700

    add header, nav button, few color sets

--- 2GeThere/HomeView.swift
+++ 2GeThere/HomeView.swift
@@ -9,21 +9,19 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        NavigationView{
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Home View")
+        NavigationView {
+            ZStack{
+                Color("primaryColor")
+                    .ignoresSafeArea()
                 
-                NavigationLink(destination: FormView()) {
-                    Text("Go to Form View")
-                }
-                NavigationLink(destination: AuthView()) {
-                    Text("Go to Auth View")
-                }
+                VStack {
+                    Header()
+                    
+                    FormButton()
+                    
+                    
+                }.padding([.leading, .trailing], 16)
             }
-            .padding()
         }
     }
 }
@@ -31,5 +29,69 @@ struct HomeView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+struct Header: View {
+    var body: some View {
+       HStack {
+            Spacer()
+            
+            VStack {
+                Text("WELCOME, HAIKAL LAZUARDI!")
+                    .font(.custom("Rubik", size: 12))
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+                Text("Afternoon Shift")
+                    .font(.custom("Rubik", size: 12))
+                    .fontWeight(.regular)
+                    .padding(.top, 6)
+                    .foregroundColor(Color("gray1"))
+            }
+            
+            NavigationLink(destination: AuthView()) {
+                VStack{
+                    Image(systemName: "rectangle.portrait.and.arrow.forward")
+                        .font(.system(size: 24.0, weight: .bold))
+                        .rotation3DEffect(.degrees(180.0), axis: (x: 0, y: 0, z: 1))
+                        .foregroundColor(.white)
+                }
+            }.offset(x: 50, y: 0)
+            
+            Spacer()
+        }
+        
+        Divider()
+            .frame(height: 1)
+            .overlay(Color("gray2"))
+        
+        .padding()
+    }
+}
+
+struct FormButton: View {
+    var body: some View {
+        NavigationLink(destination: FormView()) {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Share a ride with another Academy Learners!")
+                        .font(.custom("Rubik", size: 24))
+                        .fontWeight(.thin)
+                        .frame(maxWidth: 270, alignment: .leading)
+                        .multilineTextAlignment(.leading)
+                }
+                
+                Image(systemName: "arrow.right")
+                    .font(.system(size: 24.0, weight: .bold))
+                    .opacity(0.5)
+                    .padding(.top, 1)
+                
+            }
+            .padding([.top, .bottom], 16)
+            .padding([.leading, .trailing], 24)
+            .foregroundColor(.black)
+            .background(Color("yellow"))
+            .cornerRadius(10)
+        }
     }
 }
