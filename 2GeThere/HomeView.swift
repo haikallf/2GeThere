8790c620a6cccba43aabfaab36db8e20de35a241
haikallf <haikallzrd@gmail.com>
2023-03-18 01:03:01 +0700

    add card view and scrollview ability

--- 2GeThere/HomeView.swift
+++ 2GeThere/HomeView.swift
@@ -19,6 +19,23 @@ struct HomeView: View {
                     
                     FormButton()
                     
+                    Text("SHOWING 20 TRIPS")
+                        .font(.system(size: 12.0, weight: .medium))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(Color("gray4"))
+                        .padding()
+                    
+                    ScrollView {
+                        Card()
+                        
+                        Card()
+                        
+                        Card()
+                        
+                        Card()
+                    }
+                    
+                    Spacer()
                     
                 }.padding([.leading, .trailing], 16)
             }
@@ -64,8 +81,6 @@ struct Header: View {
         Divider()
             .frame(height: 1)
             .overlay(Color("gray2"))
-        
-        .padding()
     }
 }
 
@@ -88,10 +103,85 @@ struct FormButton: View {
                 
             }
             .padding([.top, .bottom], 16)
-            .padding([.leading, .trailing], 24)
+            .padding([.leading, .trailing], 32)
             .foregroundColor(.black)
             .background(Color("yellow"))
             .cornerRadius(10)
+        }.padding(.top)
+    }
+}
+
+struct Card: View {
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading){
+                HStack {
+                    Image(systemName: "location.north.fill")
+                        .font(.system(size: 12.0, weight: .regular))
+                    
+                    Text("LOCATION")
+                        .font(.custom("Rubik", size: 12))
+                        .fontWeight(.medium)
+                }
+                .foregroundColor(Color("gray1"))
+                
+                Spacer()
+                
+                
+                Text("13.00 - 13.10")
+                    .font(.custom("Rubik", size: 14))
+                    .fontWeight(.medium)
+                
+                Text("Raize • B 1234 XYZ")
+                    .font(.custom("Rubik", size: 12))
+                    .fontWeight(.regular)
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "person")
+                        .font(.system(size: 24.0, weight: .regular))
+                    
+                    VStack (alignment: .leading) {
+                        Text("Haikal Lazuardi")
+                            .font(.custom("Rubik", size: 12))
+                        
+                        Text("+62 81219083250")
+                            .font(.custom("Rubik", size: 12))
+                    }
+                }
+                
+                
+            }.foregroundColor(.white)
+            
+            Spacer()
+            
+            VStack {
+                Text("CAPACITY")
+                    .font(.system(size: 14.0, weight: .medium))
+                    .foregroundColor(Color("yellow"))
+                
+                Spacer()
+                
+                Text("2/6 Person")
+                    .font(.system(size: 12.0, weight: .bold))
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Text("Join")
+                    .frame(width: 92, height: 37)
+                    .font(.system(size: 14.0, weight: .medium))
+                    .foregroundColor(Color("yellow"))
+                    .background(Color("primaryColor"))
+                    .cornerRadius(15)
+                    
+                
+            }
         }
+        .frame(width: 305, height: 100)
+        .padding()
+        .background(Color("gray3"))
+        .cornerRadius(15)
     }
 }
